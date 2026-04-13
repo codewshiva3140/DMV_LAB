fe8e1e4633566bf122944185dfad4caf886ee67a
Stark <shivakumarpie@gmail.com>
2026-04-13 12:23:03 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5, 6]
+y = [10, 15, 13, 17, 20, 22]
+
+# Create scatter plot
+plt.scatter(x, y)
+
+# Add labels and title
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+plt.title("Simple Scatter Plot")
+
+# Show plot
+plt.show()
